470816000e7623a861d485d840291eb336153e3a
lmotychenkov <motychenkovLA@gmail.com>
2022-01-18 18:11:45 +0300

    дз 2 замечания

--- src/main/java/helloWorld/Main.java
+++ src/main/java/helloWorld/Main.java
@@ -7,10 +7,16 @@ public class Main {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите резюме");
         String resume = console.nextLine();
+        // todo 1 - "выдать список вариантов" для критичностей
         System.out.println("Введите критичность дефекта");
         String critical = console.nextLine();
         System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+        // todo 1 - lowerCamelCase, numberOfDays
         int NUMBER_OF_DAY = console.nextInt();
+        // todo 3 - здесь console.nextLine(), чтобы потом не забыть
+        // todo 1 - lowerCamelCase, moreThanWeek
+        // todo 0 - рабочая неделя 5 дней
+        // todo 3 - лучше вынести 7 (5) в именованную константу в начало main
         boolean MORE_THAN_7 = NUMBER_OF_DAY>7;
         System.out.println("Резюме:" + resume + "|" + "Критичность:" + critical + "|" +"Дней на исправление:" + NUMBER_OF_DAY + "|" + MORE_THAN_7);
 
