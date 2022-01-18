e582adbf1e5c5ff8c0c2d1728269ad9becd37270
valkiriya <popova.daria.sergeevna@gmail.com>
2022-01-18 18:54:21 +0300

    Домашнее задание №2 Исправлено: - Добавлена константа для кол-ва дней в рабочую неделю; - Выход за правую крайнюю линию убрала; - Добавлена переменная boolean.

--- src/main/java/bugTracker/Main.java
+++ src/main/java/bugTracker/Main.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //task_2 18/01/22
+        final int workDaysInWeek= 5;
+
         System.out.println("Введите резюме дефекта: ");
         Scanner scanner = new Scanner(System.in);
         String defectSummary = scanner.nextLine();
-        System.out.println("Введите критичность дефекта, например:\nP1 – Высокая (High) – требуется исправить в первую очередь;\n" +
-                "P2 – Средняя (Medium) – требуется исправить во вторую очередь, когда нет дефектов с высоким приоритетом;\n" +
-                "P3 – Низкая (Low) – исправляется в последнюю очередь, когда все дефекты с более высоким приоритетом уже исправлены.");
+        System.out.println("Введите критичность дефекта, например:\nP1 – Высокая (High);\n" +
+                "P2 – Средняя (Medium);\nP3 – Низкая (Low).");
         String defectPriority = scanner.nextLine();
         System.out.println("Введите ожидаемое количество дней на исправление дефекта: ");
         int defectDayCount = scanner.nextInt();
-        String weekResult = defectDayCount > 5 ? "\nИсправление займет больше рабочей недели" : "\nИсправление в рамках рабочей недели";
+        boolean moreThanWeek = defectDayCount > workDaysInWeek;
 
-        System.out.println("Резюме: " + defectSummary + "\nСерьезность: " + defectPriority + "\nКоличество дней на исправление: " +
-                defectDayCount + weekResult);
+        System.out.println("Резюме: " + defectSummary + "\nСерьезность: " + defectPriority +
+                "\nКоличество дней на исправление: " + defectDayCount +
+                "\nЗаймет больше рабочей недели: " + moreThanWeek);
 
 
     }
