8b7f06a0e0e9c59facfbcc2838f8f70cf8e6c6df
valkiriya <popova.daria.sergeevna@gmail.com>
2022-01-18 18:59:47 +0300

    Домашнее задание №2 Исправлено: - Константа капсом через нижнее подчеркивание

--- src/main/java/bugTracker/Main.java
+++ src/main/java/bugTracker/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //task_2 18/01/22
-        final int workDaysInWeek= 5;
+        final int WORK_DAYS_IN_WEEK = 5;
 
         System.out.println("Введите резюме дефекта: ");
         Scanner scanner = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class Main {
         String defectPriority = scanner.nextLine();
         System.out.println("Введите ожидаемое количество дней на исправление дефекта: ");
         int defectDayCount = scanner.nextInt();
-        boolean moreThanWeek = defectDayCount > workDaysInWeek;
+        boolean moreThanWeek = defectDayCount > WORK_DAYS_IN_WEEK;
 
         System.out.println("Резюме: " + defectSummary + "\nСерьезность: " + defectPriority +
                 "\nКоличество дней на исправление: " + defectDayCount +
