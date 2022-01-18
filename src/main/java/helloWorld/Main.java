f54d257440d8ef55f31f18efd663010c6d795383
MaxSalimgareev <salimgareev_maks@mail.ru>
2022-01-18 18:19:38 +0300

    ДЗ 2 исправленная

--- src/main/java/helloWorld/Main.java
+++ src/main/java/helloWorld/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int workweek = 5;
         Scanner console = new Scanner(System.in);
         System.out.println("Введите резюме");
         String resume = console.nextLine();
@@ -12,13 +13,14 @@ public class Main {
         String critical = console.nextLine();
         System.out.println("Введите ожидаемое количество дней на исправление дефекта");
         // todo 1 - lowerCamelCase, numberOfDays
-        int NUMBER_OF_DAY = console.nextInt();
+        int numberOfDays = console.nextInt();
+        console.nextLine();
         // todo 3 - здесь console.nextLine(), чтобы потом не забыть
         // todo 1 - lowerCamelCase, moreThanWeek
         // todo 0 - рабочая неделя 5 дней
         // todo 3 - лучше вынести 7 (5) в именованную константу в начало main
-        boolean MORE_THAN_7 = NUMBER_OF_DAY>7;
-        System.out.println("Резюме:" + resume + "|" + "Критичность:" + critical + "|" +"Дней на исправление:" + NUMBER_OF_DAY + "|" + MORE_THAN_7);
+        boolean moreThanWeek = numberOfDays>workweek;
+        System.out.println("Резюме:" + resume + "|" + "Критичность:" + critical + "|" +"Дней на исправление:" + numberOfDays + "|" + moreThanWeek);
 
     }
 }
