15ad5ce1725110f12fd8a0f075d843a2b0071312
MaxSalimgareev <salimgareev_maks@mail.ru>
2022-01-18 17:20:11 +0300

    ДЗ 2

--- src/main/java/helloWorld/Main.java
+++ src/main/java/helloWorld/Main.java
@@ -1,7 +1,18 @@
 package helloWorld;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите резюме");
+        String resume = console.nextLine();
+        System.out.println("Введите критичность дефекта");
+        String critical = console.nextLine();
+        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+        int NUMBER_OF_DAY = console.nextInt();
+        boolean MORE_THAN_7 = NUMBER_OF_DAY>7;
+        System.out.println("Резюме:" + resume + "|" + "Критичность:" + critical + "|" +"Дней на исправление:" + NUMBER_OF_DAY + "|" + MORE_THAN_7);
+
     }
 }
