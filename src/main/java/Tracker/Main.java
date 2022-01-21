5991acd49e21c49310de32754e404dd8d284df66
stupaDV <stupadmitry@gmail.com>
2022-01-21 20:01:04 +0300

    Домашнее задание 3

--- src/main/java/Tracker/Main.java
+++ src/main/java/Tracker/Main.java
@@ -4,20 +4,52 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        int menuItem = 0;
+        int i = 0;
         final int WORK_WEEK = 5;
-
-
+        String[] defect;
+        defect = new String[10];
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите описание дефекта");
-        String resume = scanner.nextLine();
-        System.out.println("Введите критичность дефекта: Low, Mid, High, Crit");
-        String criticality = scanner.nextLine();
-        System.out.println("Введите ожидаемое количество дней на исправление");
-        int daysToFix = scanner.nextInt();
-        boolean fixInAWeek = daysToFix < WORK_WEEK;
-        System.out.println(resume + " | " + criticality + " | " + daysToFix + " | "+ fixInAWeek);
 
+        while (menuItem != 3){
+            System.out.println("Выберите действие(укажите номер пункта меню): \n" +
+                    "1.Добавить дефект\n" +
+                    "2.Вывести список дефектов\n" +
+                    "3.Выход");
+            menuItem = scanner.nextInt();
+            scanner.nextLine();
+            switch (menuItem){
+                case 1: if (i < defect.length) {
+                    System.out.println("Введите описание дефекта");
+                    String resume = scanner.nextLine();
+                    System.out.println("Введите критичность дефекта: Low, Mid, High, Critical");
+                    String criticality = scanner.nextLine();
+                    System.out.println("Введите ожидаемое количество дней на исправление");
+                    int daysToFix = scanner.nextInt();
+                    boolean fixInAWeek = daysToFix < WORK_WEEK;
+                    defect[i] = resume + " | " + criticality + " | " + daysToFix + " | " + fixInAWeek;
+                    i++;
+                }
+                else System.out.println("Превышено максимальное количество дефектов!");
+                    System.out.println();
+                    break;
+
+                case 2:
+                    System.out.println( "Описание дефекта | Критичность | Дней на исправление | Займет меньше рабочей недели");
+                    for (int j = 0;j < i;j++) {
+                        System.out.println(defect[j]);
+                    }
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    System.out.println("Такого номера нет!") ;
+                    System.out.println();
+            }
+
+        }
     }
 }
