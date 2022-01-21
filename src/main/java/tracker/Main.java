3c3a1ad11b8908ef0baeebd4619ab2dbd61a74b3
Armandy <kamelija_2000@mail.ru>
2022-01-21 17:53:13 +0300

    Домашнее задание 3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -6,23 +6,61 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        final int WORK_WEEK = 5;
+
+        final int WORK_WEEK = 5; // константа, кол-во рабочих дней
+        final int MAX_BUG = 10;  //константа, максимальное кол-во дефектов
+
+        boolean isRun = true; // показатель того, в работе мы или идем на выход
+        int  countBug = 0; //кол-во заведенных дефектов
+
+        String[] defects  = new String[MAX_BUG]; //массив дефектов
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите резюме дефекта");
-        String resumeBug = scanner.nextLine();
+        while (isRun) {
+            System.out.println(" Выберите действие " +
+                    "\n add (Добавить новый дефект)" +
+                    "\n list (Вывести список дефектов)" +
+                    "\n quit (Вернуться в главное меню)");
 
-        System.out.println("Введите критичность дефекта (Low, Medium, High)");
-        String criticalBug = scanner.nextLine();
+            String action = scanner.nextLine();
+            switch (action) {
+                case ("add"): {
+                    if (countBug > MAX_BUG - 1) {
+                        System.out.println("В списке есть уже " + MAX_BUG + " дефектов, больше ввести не возможно!");
+                    }else {
+                        System.out.println("Введите резюме дефекта");
+                        String resumeBug = scanner.nextLine();
 
-        System.out.println("Введите ожидаемое кол-во дней на исправление дефекта");
-        int dayCountBug = scanner.nextInt();
-        scanner.nextLine();
+                        System.out.println("Введите критичность дефекта (Low, Medium, High)");
+                        String criticalBug = scanner.nextLine();
 
-        boolean compareWeek = dayCountBug > WORK_WEEK;
+                        System.out.println("Введите ожидаемое кол-во дней на исправление дефекта");
+                        int dayCountBug = scanner.nextInt();
+                        scanner.nextLine();
 
-        System.out.println("Резюме  | Серьезность | Кол-во дней на исправление | займет больше рабочей недели (true/false)");
-        System.out.println(resumeBug + "  |  " + criticalBug + "  |  " + dayCountBug + "  |  " + compareWeek);
+                        defects[countBug] = countBug + "  |  " + resumeBug + "  |  " + criticalBug + "  |  " + dayCountBug;
+                        countBug++;
+
+                    }
+                    break;
+                }
+                case ("list"): {
+                    System.out.println(" Номер |  Резюме  | Серьезность | Кол-во дней на исправление ");
+                    for (int i = 0; i < countBug; i++) {
+                        System.out.println(defects[i]);
+                    }
+                    break;
+                }
+                case ("quit"): {
+                    isRun = false;
+                    break;
+                }    }
+        }
+
+
+
+
 
     }
 }
