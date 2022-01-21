f175fc0092130c95cb3bfab1b80f4129a439e6f7
MaxSalimgareev <salimgareev_maks@mail.ru>
2022-01-21 18:36:58 +0300

    ДЗ 3 исправленная

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         boolean run = true;
         int numberOfDefects = 0;
-        final int arraySize = 10; // todo 1 - CONST_STYLE
-        String[] resumeArray = new String[arraySize];
-        String[] criticalArray = new String[arraySize];
-        int[] numberOfDaysArray = new int[arraySize];
+        final int CONST_STYLE = 10;
+        String[] resumeArray = new String[CONST_STYLE];
+        String[] criticalArray = new String[CONST_STYLE];
+        int[] numberOfDaysArray = new int[CONST_STYLE];
         while (run) {
             Scanner console = new Scanner(System.in);
             System.out.println("Чтобы добавить новый дефект, введите \"add\". Чтобы вывести список дефектов, введите \"list\". Чтобы выйти, введите \"quit\"");
@@ -21,21 +21,18 @@ public class Main {
                     }
                     break;
                 case "add":
-                    if (numberOfDefects == arraySize) { // todo ? - можно совсем уж на всякий случай >= сделать
+                    if (numberOfDefects >= CONST_STYLE) {
                         System.out.println("Невозможно добавить больше 10 дефектов");
                         break;
                     }
-                    if (resumeArray[numberOfDefects] == null) { // todo 3 - это условие всегда true
-                        System.out.println("Введите резюме");
-                        resumeArray[numberOfDefects] = console.nextLine();
-
-                        System.out.println("Введите критичность дефекта: критично, некритично или *****");
-                        criticalArray[numberOfDefects] = console.nextLine();
-                        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
-                        numberOfDaysArray[numberOfDefects] = console.nextInt();
-                        console.nextLine();
-                        numberOfDefects++;
-                    }
+                    System.out.println("Введите резюме");
+                    resumeArray[numberOfDefects] = console.nextLine();
+                    System.out.println("Введите критичность дефекта: критично, некритично или *****");
+                    criticalArray[numberOfDefects] = console.nextLine();
+                    System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+                    numberOfDaysArray[numberOfDefects] = console.nextInt();
+                    console.nextLine();
+                    numberOfDefects++;
                     break;
                 case "quit":
                     run = false;
