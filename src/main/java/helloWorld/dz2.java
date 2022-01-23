f6fabe9c883bc08f45ee8408d14ba80c8745e5e5
acart <acart1708@gmail.com>
2022-01-24 00:37:04 +0300

    дз2

--- src/main/java/helloWorld/dz2.java
+++ src/main/java/helloWorld/dz2.java
@@ -10,8 +10,8 @@ public class dz2 { public static void  main (String[] args) {
     String Critical = scanner.nextLine();
             System.out.println("Введите ожидаемое количество дней на исправление дефекта");
 int NumberOfDays = scanner.nextInt();
-    int DaysOfWeek=5;
-    boolean WorkWeek = NumberOfDays>=DaysOfWeek;
+    int DaysOfWorkWeek=5;
+    boolean WorkWeek = NumberOfDays>=DaysOfWorkWeek;
     System.out.println(Description + " | " + Critical + " | " + NumberOfDays + " | Займёт больше рабочей недели:" + WorkWeek);
 
 
