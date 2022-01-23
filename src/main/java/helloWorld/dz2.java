b0bd08e7d2cb2210a203e8a6d6515efdd03ed8da
acart <acart1708@gmail.com>
2022-01-23 23:15:56 +0300

    дз1

---
+++ src/main/java/helloWorld/dz2.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+import java.util.Scanner;
+
+public class dz2 { public static void  main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите резюме дефекта");
+    String Description = scanner.nextLine();
+    System.out.println("Введите критичность дефекта:\nLow\nMedium\nHigh\nCritical");
+    String Critical = scanner.nextLine();
+            System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+int NumberOfDays = scanner.nextInt();
+    int DayOfWeek=5;
+    boolean WorkWeek = NumberOfDays>=DayOfWeek;
+    System.out.println(Description + "|" + Critical + "|" + NumberOfDays + "| Займёт больше рабочей недели:" + WorkWeek);
+
+}
+}
