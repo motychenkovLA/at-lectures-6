ce5b29166a91465960b99d0cd7da5c7d625083cf
acart <acart1708@gmail.com>
2022-01-25 13:12:42 +0300

    дз2

--- src/main/java/helloWorld/dz2.java
+++ src/main/java/helloWorld/dz2.java
@@ -2,20 +2,21 @@ package helloWorld;
 
 import java.util.Scanner;
 
-public class dz2 { public static void  main (String[] args) {
-    Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите резюме дефекта");
-    String Description = scanner.nextLine();
-    System.out.println("Введите критичность дефекта:\nLow\nMedium\nHigh\nCritical");
-    String Critical = scanner.nextLine();
-            System.out.println("Введите ожидаемое количество дней на исправление дефекта");
-int NumberOfDays = scanner.nextInt();
-    int DaysOfWorkWeek=5;
-    boolean WorkWeek = NumberOfDays>DaysOfWorkWeek;
-    System.out.println(Description + " | " + Critical + " | " + NumberOfDays + " | Займёт больше рабочей недели:" + WorkWeek);
+public class dz2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта");
+        String description = scanner.nextLine();
+        System.out.println("Введите критичность дефекта:\nLow\nMedium\nHigh\nCritical");
+        String critical = scanner.nextLine();
+        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+        int daysNumber = scanner.nextInt();
+        int workWeek = 5;
+        boolean moreThanWorkWeek = daysNumber >= workWeek;
+        System.out.println(description + " | " + critical + " | " + daysNumber + " | Займёт больше рабочей недели:" + moreThanWorkWeek);
 
 
-}
+    }
 }
 
 
