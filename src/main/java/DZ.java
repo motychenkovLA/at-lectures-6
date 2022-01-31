c6cad0ac535e46d097f2c80ba147d9d5f373722e
Pronnikov <apronnikov@t1-consulting.ru>
2022-01-31 16:39:59 +0300

    замечания дз3

--- src/main/java/DZ.java
+++ src/main/java/DZ.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class DZ {
 public static void main(String[] args) {
+    //todo убрать второй сканер
         Scanner scanner = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
+        //todo убрать условие сделать wile true
         String command = "r";
+        //todo нейминг                      \/ магическое число
+        //todo разбить на 3 массива
         String[] arrayDefects = new String[10];
         int countDefects = 0;
+
 
         while(!command.equals("Quit")) {
             System.out.println("Главное меню : \n1.Add\n2.List\n3.Quit");
             System.out.println("Введите команду:");
-            command = scanner.nextLine();
+            command = sc.nextLine();
             if (command.equals("Add")) {
                 if(countDefects < arrayDefects.length) {
                     System.out.println("Введите резюме дефекта:");
@@ -31,12 +36,14 @@ public static void main(String[] args) {
                     System.out.println("Хранилище может содержать не более 10 дефектов");
             }
             else if (command.equals("List")) {
+                //todo вывести только заполненные
                 for (int i = 0; i < arrayDefects.length; i++) {
 
                     System.out.println(arrayDefects[i]);
                 }
             }
             else if (command.equals("Quit")) {
+                //todo сказать досводиния
                 break;
             }
             else {
