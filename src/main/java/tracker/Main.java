e79d60409aee861fe6458abadec59e2188192018
MaxSalimgareev <salimgareev_maks@mail.ru>
2022-02-02 18:27:20 +0300

    ДЗ 4

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -7,18 +7,18 @@ public class Main {
         boolean run = true;
         int numberOfDefects = 0;
         final int CONST_STYLE = 10;
-        String[] resumeArray = new String[CONST_STYLE];
-        String[] criticalArray = new String[CONST_STYLE];
-        int[] numberOfDaysArray = new int[CONST_STYLE];
+        String resume;
+        String critical;
+        int numberOfDays;
+        Defect defectList = new Defect();
+        defectList.setConstStyle(CONST_STYLE);
         while (run) {
             Scanner console = new Scanner(System.in);
             System.out.println("Чтобы добавить новый дефект, введите \"add\". Чтобы вывести список дефектов, введите \"list\". Чтобы выйти, введите \"quit\"");
             String action = console.nextLine();
             switch (action) {
                 case "list":
-                    for (int i = 0; i < numberOfDefects; i++) {
-                        System.out.println("" + i + "|" + "Резюме:" + resumeArray[i] + "|" + "Критичность:" + criticalArray[i] + "|" + "Дней на исправление:" + numberOfDaysArray[i]);
-                    }
+                    defectList.getDefects();
                     break;
                 case "add":
                     if (numberOfDefects >= CONST_STYLE) {
@@ -26,12 +26,13 @@ public class Main {
                         break;
                     }
                     System.out.println("Введите резюме");
-                    resumeArray[numberOfDefects] = console.nextLine();
+                    resume = console.nextLine();
                     System.out.println("Введите критичность дефекта: критично, некритично или *****");
-                    criticalArray[numberOfDefects] = console.nextLine();
+                    critical = console.nextLine();
                     System.out.println("Введите ожидаемое количество дней на исправление дефекта");
-                    numberOfDaysArray[numberOfDefects] = console.nextInt();
+                    numberOfDays = console.nextInt();
                     console.nextLine();
+                    defectList.defect(numberOfDefects, resume, critical, numberOfDays);
                     numberOfDefects++;
                     break;
                 case "quit":
