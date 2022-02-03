fb4902daed568e3c5739c0e28bdc870b38a25e10
lmotychenkov <motychenkovLA@gmail.com>
2022-02-03 15:37:24 +0300

    дз 4 замечания

--- src/main/java/tracker/Defect.java
+++ src/main/java/tracker/Defect.java
@@ -2,8 +2,10 @@ package tracker;
 
 
 public class Defect {
+    // todo 3 - нет полей, хранится только текстовое представление
     String defect;
 
+    // todo 3 - между созданием и вызовом addDefect, объект существует в не валидном состоянии с null-ом
     void addDefect(int numberOfDefects, String resume, String critical, int numberOfDays) {
         defect = "" + numberOfDefects + " | " + resume + " | " + critical + " | " + numberOfDays;
     }
