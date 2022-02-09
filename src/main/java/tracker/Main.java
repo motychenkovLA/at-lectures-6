83f2a7a50a63fd3a889884b94c470a6d4d0834a8
tburmatova <burmatova98@gmail.com>
2022-02-09 21:51:41 +0300

    Домашнее задание 5

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,22 +1,21 @@
 package tracker;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Repository repository = new Repository(10);
         Scanner scanner = new Scanner(System.in);
-        final int maxBugs = 10;
-        Defect[] defectList = new Defect[maxBugs];
-        int idBug = 0; // todo 1 - не используется
         String command = null;
-        int countBugs = 0;
+
         while (!Objects.equals(command, "quit")) {
             System.out.println("Введите: add, list или quit");
             command = scanner.nextLine();
             switch (command) {
                 case "add":
-                    if (countBugs < maxBugs) {
+                    if (Defect.getCountDefects() < repository.maxBugs) {
                         System.out.println("Введите резюме дефекта");
                         String resumeBug = scanner.nextLine();
                         System.out.println("Введите критичность дефекта");
@@ -25,25 +24,20 @@ public class Main {
                         int daysToFixBug = scanner.nextInt();
                         scanner.nextLine();
 
-                        defectList[countBugs] = new Defect(countBugs + 1, resumeBug, severityBug, daysToFixBug);
-                        countBugs++;
+                        repository.addDefect(Defect.getCountDefects(), new Defect(resumeBug, severityBug,
+                                daysToFixBug));
+
                     } else {
                         System.out.println("Превышено максимально допустимое кол-во дефектов");
                     }
                     break;
 
                 case "list":
-                    for (int a = 0; a < countBugs; a++) {
-                        System.out.println(defectList[a].getBugsInfo());
+                    for (int a = 0; a < Defect.getCountDefects(); a++) {
+                        System.out.println(Arrays.toString(repository.getAllDefects()));
                     }
                     break;
 
-                case "quit":
-                    break;
-
-                default:
-                    System.out.println("Введено не корректное значение, повторите попытку");
-                    break;
 
             }
         }
--- src/main/java/tracker/Repository.java
+++ src/main/java/tracker/Repository.java
@@ -3,14 +3,12 @@ package tracker;
 public class Repository {
     private Defect[] defectsList;
     public int maxBugs;
-
     public Repository(int maxBugs) {
         this.maxBugs = maxBugs;
         defectsList = new Defect[maxBugs];
     }
 
     public void addDefect(int id, Defect defect) {
-
         defectsList[Defect.getCountDefects()] = defect;
     }
 
