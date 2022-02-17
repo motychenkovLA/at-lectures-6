f52eedda666d5c113ddcd1a12a9eb1d7bd849191
acart <acart1708@gmail.com>
2022-02-17 20:44:49 +0300

    дз4

--- src/main/java/helloWorld/Defect.java
+++ src/main/java/helloWorld/Defect.java
@@ -9,15 +9,15 @@ public class Defect {
     static private int counter = 1;
 
     public Defect(String description, String critical, int daysNumber) {
+        id = counter++;
         this.description = description;
         this.critical = critical;
         this.daysNumber = daysNumber;
-        id = counter++;
+
     }
 
     String getInfo() {
-        return id + " | " + description + "|  " + critical +
-                " | " + daysNumber;
+        return id + " | " + description + "|  " + critical + " | " + daysNumber;
     }
 }
 
