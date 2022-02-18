037f0e6381b5b794c02d8d8d2a4620ef9823a1bc
Pronnikov <apronnikov@t1-consulting.ru>
2022-02-18 15:04:01 +0300

    замечания дз 6

--- src/main/java/Tracker/Attachment.java
+++ src/main/java/Tracker/Attachment.java
@@ -2,8 +2,10 @@ package Tracker;
 
 public abstract class Attachment {
 
+    //todo здесь это не нужно, поля необходимо перенести в наследники, для коментария String, для дефекта int
     String attachment;
 
+    //todo в этом конструкторе надобность также отпадает, у каждого наследника свой конструктор
     public Attachment(String attachment) {
         this.attachment = attachment;
     }
