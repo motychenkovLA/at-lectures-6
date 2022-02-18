ff476d71c97dd1d4f58787ceea28a32cf6373d9b
acart <acart1708@gmail.com>
2022-02-18 12:16:05 +0300

    дз5

--- src/main/java/helloWorld/Repository.java
+++ src/main/java/helloWorld/Repository.java
@@ -9,8 +9,8 @@ public class Repository {
 
     static public void add(Defect def) {
 
-        if (countOfBug > size - 100) {
-            size = size + 10000;
+        if (countOfBug > size - 10) {
+            size = size + 100;
             Defect[] scale = listBug;
             listBug = new Defect[size];
             System.arraycopy(scale, 0, listBug, 0, scale.length);
