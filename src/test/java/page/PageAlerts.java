be4fddc68fd86553ce8a3a8cf8e492fc266e4dfc
Kristina Zakharova <unsinn1989@mail.ru>
2022-05-04 13:10:45 +0300

    Домашнее задание 15. Исправление замечаний

--- src/test/java/page/PageAlerts.java
+++ src/test/java/page/PageAlerts.java
@@ -13,35 +13,38 @@ public class PageAlerts {
     private WebDriver webDriver;
 
 
-    private By alertButtonxpath = By.xpath("//button[@id ='alertButton']");
-    private By timerAlertButtonxpath = By.xpath("//button[@id ='timerAlertButton']");
-    private By confirmButtonxpath = By.xpath("//button[@id ='confirmButton']");
+    private By alertButtonXpath = By.xpath("//button[@id ='alertButton']");
+    private By timerAlertButtonXpath = By.xpath("//button[@id ='timerAlertButton']");
+    private By confirmButtonXpath = By.xpath("//button[@id ='confirmButton']");
+    private By confirmResultXpath = By.xpath("//span[@id ='confirmResult']");
 
     public PageAlerts(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
     public void clickAlertButton() {
-        WebElement alertButton = webDriver.findElement(alertButtonxpath);
+
+        WebElement alertButton = webDriver.findElement(alertButtonXpath);
         alertButton.click();
         webDriver.switchTo().alert().accept();
     }
 
+
     public void clickTimerAlertButton() {
-        WebElement timerAlertButton = webDriver.findElement(timerAlertButtonxpath);
+        WebElement timerAlertButton = webDriver.findElement(timerAlertButtonXpath);
         timerAlertButton.click();
         WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
         webDriverWait.until(ExpectedConditions.alertIsPresent()).accept();
     }
 
     public void clickConfirmButton() {
-        WebElement confirmButton = webDriver.findElement(confirmButtonxpath);
+        WebElement confirmButton = webDriver.findElement(confirmButtonXpath);
         confirmButton.click();
         webDriver.switchTo().alert().dismiss();
     }
 
     public boolean isHaveConfirmResult() {
-        return !webDriver.findElements(By.xpath("//span[@id ='confirmResult']")).isEmpty();
+        return !webDriver.findElements(confirmResultXpath).isEmpty();
     }
 
 
--- src/test/java/page/PageButtons.java
+++ src/test/java/page/PageButtons.java
@@ -13,6 +13,9 @@ public class PageButtons {
     private By doubleClickxpath = By.xpath("//button[text()='Double Click Me']");
     private By rightClickxpath = By.xpath("//button[text()='Right Click Me']");
     private By clickxpath = By.xpath("//button[text()='Click Me']");
+    private By doubleClickTextXpath = By.xpath("//p[text()='You have done a double click']");
+    private By rightClickTextXpath = By.xpath("//p[text()='You have done a right click']");
+    private By clickTextXpath = By.xpath("//p[text()='You have done a dynamic click']");
 
     public PageButtons(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -41,15 +44,15 @@ public class PageButtons {
     }
 
     public boolean isHaveDoubleClickText() {
-        return !webDriver.findElements(By.xpath("//p[text()='You have done a double click']")).isEmpty();
+        return !webDriver.findElements(doubleClickTextXpath).isEmpty();
     }
 
     public boolean isHaveRightClickText() {
-        return !webDriver.findElements(By.xpath("//p[text()='You have done a right click']")).isEmpty();
+        return !webDriver.findElements(rightClickTextXpath).isEmpty();
     }
 
     public boolean isHaveClickText() {
-        return !webDriver.findElements(By.xpath("//p[text()='You have done a dynamic click']")).isEmpty();
+        return !webDriver.findElements(clickTextXpath).isEmpty();
     }
 
 }
